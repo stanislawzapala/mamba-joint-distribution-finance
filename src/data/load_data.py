6c139895ecb52470a87064f25c23fb0f904c8bb4
Stasimon <stanislaw.i.zapala@gmail.com>
2026-07-01 12:42:50 +0200

    restructuring folder, adding load_data function

---
+++ src/data/load_data.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+from pathlib import Path
+
+
+files_to_load = sorted(Path("djia29").glob("*.csv"))
+
+
+### Data Preprocessing ###
+
+def load_raw_djia(folder_path: str = 'data/raw'):
+    """
+    Load raw data from CSV files in a folder.
+
+    Parameters:
+    folder_path (str): The path to the folder containing CSV files.
+
+    Returns:
+    dictionary: A dictionary where keys are stock symbols and values are DataFrames containing the stock data.
+    """
+
+    path = Path(folder_path)
+    csv_files = path.glob("*.csv")
+
+    data_dict = {}
+
+    for file in csv_files:
+        ticker = file.stem
+        df = pd.read_csv(file, index_col='date', parse_dates=['date'], skiprows=[1])
+        data_dict[ticker] = df
+
+    return data_dict
+
+
+    
+
